ba5e299ffe4d0d576bd8e722e3f6c1441f95c5d6
Al Viro <viro@zeniv.linux.org.uk>
2012-06-09 00:59:08 -0400

    get rid of ->mnt_longterm
    it's enough to set ->mnt_ns of internal vfsmounts to something
    distinct from all struct mnt_namespace out there; then we can
    just use the check for ->mnt_ns != NULL in the fast path of
    mntput_no_expire()
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    (cherry picked from commit f7a99c5b7c8bd3d3f533c8b38274e33f3da9096e)
    
    Conflicts:
    	fs/dcache.c

--- fs/dcache.c
+++ fs/dcache.c
@@ -2510,27 +2510,14 @@ static int prepend_path(const struct path *path,
 	struct dentry *dentry = path->dentry;
 	struct vfsmount *vfsmnt = path->mnt;
 	struct mount *mnt = real_mount(vfsmnt);
-	char *orig_buffer = *buffer;
-	int orig_len = *buflen;
 	bool slash = false;
 	int error = 0;
 
-<<<<<<< HEAD
-=======
 	br_read_lock(&vfsmount_lock);
->>>>>>> a255431... brlocks/lglocks: API cleanups
 	while (dentry != root->dentry || vfsmnt != root->mnt) {
 		struct dentry * parent;
 
 		if (dentry == vfsmnt->mnt_root || IS_ROOT(dentry)) {
-			/* Escaped? */
-			if (dentry != vfsmnt->mnt_root) {
-				*buffer = orig_buffer;
-				*buflen = orig_len;
-				slash = false;
-				error = 3;
-				goto global_root;
-			}
 			/* Global root? */
 			if (!mnt_has_parent(mnt))
 				goto global_root;
@@ -2556,11 +2543,8 @@ static int prepend_path(const struct path *path,
 	if (!error && !slash)
 		error = prepend(buffer, buflen, "/", 1);
 
-<<<<<<< HEAD
-=======
 out:
 	br_read_unlock(&vfsmount_lock);
->>>>>>> a255431... brlocks/lglocks: API cleanups
 	return error;
 
 global_root:
@@ -2576,8 +2560,8 @@ global_root:
 	if (!slash)
 		error = prepend(buffer, buflen, "/", 1);
 	if (!error)
-		error = real_mount(vfsmnt)->mnt_ns ? 1 : 2;
-	return error;
+		error = is_mounted(vfsmnt) ? 1 : 2;
+	goto out;
 }
 
 /**
@@ -2604,11 +2588,9 @@ char *__d_path(const struct path *path,
 	int error;
 
 	prepend(&res, &buflen, "\0", 1);
-	br_read_lock(vfsmount_lock);
 	write_seqlock(&rename_lock);
 	error = prepend_path(path, root, &res, &buflen);
 	write_sequnlock(&rename_lock);
-	br_read_unlock(vfsmount_lock);
 
 	if (error < 0)
 		return ERR_PTR(error);
@@ -2625,11 +2607,9 @@ char *d_absolute_path(const struct path *path,
 	int error;
 
 	prepend(&res, &buflen, "\0", 1);
-	br_read_lock(vfsmount_lock);
 	write_seqlock(&rename_lock);
 	error = prepend_path(path, &root, &res, &buflen);
 	write_sequnlock(&rename_lock);
-	br_read_unlock(vfsmount_lock);
 
 	if (error > 1)
 		error = -EINVAL;
@@ -2693,13 +2673,11 @@ char *d_path(const struct path *path, char *buf, int buflen)
 		return path->dentry->d_op->d_dname(path->dentry, buf, buflen);
 
 	get_fs_root(current->fs, &root);
-	br_read_lock(vfsmount_lock);
 	write_seqlock(&rename_lock);
 	error = path_with_deleted(path, &root, &res, &buflen);
-	write_sequnlock(&rename_lock);
-	br_read_unlock(vfsmount_lock);
 	if (error < 0)
 		res = ERR_PTR(error);
+	write_sequnlock(&rename_lock);
 	path_put(&root);
 	return res;
 }
@@ -2854,7 +2832,6 @@ SYSCALL_DEFINE2(getcwd, char __user *, buf, unsigned long, size)
 	get_fs_root_and_pwd(current->fs, &root, &pwd);
 
 	error = -ENOENT;
-	br_read_lock(vfsmount_lock);
 	write_seqlock(&rename_lock);
 	if (!d_unlinked(pwd.dentry)) {
 		unsigned long len;
@@ -2864,7 +2841,6 @@ SYSCALL_DEFINE2(getcwd, char __user *, buf, unsigned long, size)
 		prepend(&cwd, &buflen, "\0", 1);
 		error = prepend_path(&pwd, &root, &cwd, &buflen);
 		write_sequnlock(&rename_lock);
-		br_read_unlock(vfsmount_lock);
 
 		if (error < 0)
 			goto out;
@@ -2885,7 +2861,6 @@ SYSCALL_DEFINE2(getcwd, char __user *, buf, unsigned long, size)
 		}
 	} else {
 		write_sequnlock(&rename_lock);
-		br_read_unlock(vfsmount_lock);
 	}
 
 out:
